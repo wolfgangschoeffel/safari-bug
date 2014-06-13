72e531bdc3a733c274c0a768dc96c68a06d5f543
wolfgangschoeffel <info@wolfgang-schoeffel.de>
2014-06-13 17:01:43 +0200

    update demo

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-var offset = 500;
+var offset = 500,
+    open = true,
+    content = document.getElementById('content');
 
-var content = document.getElementById('content');
 content.style.marginTop = offset + 'px';
 
-
 window.addEventListener('scroll', function(e){
-    console.log('scroll');
+    
+    if( !open ){
+        return;
+    }
+
     var top = window.pageYOffset || document.documentElement.scrollTop;
 
     if( top > offset ){
 
         content.style.marginTop = 0;
         window.scrollTo(0,0);
-        
+        open = false;
+
     }
 
 });
